5f411a3589657a85c224fc96c54149372e398c43
drout17 <drout17@gmail.com>
2022-12-29 15:29:10 +0530

    Removed Modal

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import './App.css';
 import Body from './Components/Body/Body';
-import UserModal from './Components/Modal/UserModal';
-import OverLay from './Components/Modal/OverLay';
 
 
 function App() {
   return (
     <>
         <Body/>
-        <UserModal/>
-       <OverLay/>
     </>
   );
 }
